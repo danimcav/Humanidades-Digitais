91b0817db558abd66eb90e18acf003951a4d088a
danimcav <145656583+danimcav@users.noreply.github.com>
2023-12-09 10:18:29 +0000

    Add files via upload

--- Folha8.py
+++ Folha8.py
@@ -7,7 +7,26 @@
 #Aluna: Daniella Monteiro Cavalheiro
 #Matricula: PG54506
 #
+import spacy
+from collections import Counter
 import re
+import matplotlib.pyplot as plt
+
+# Função para realizar NER apenas em entidades entre chaves {}
+def realizar_ner_entre_chaves(texto):
+    entidades_chaves = re.findall(r'{(.*?)}', texto)
+    return entidades_chaves
+
+# Função para extrair POS tags das palavras nas tags {}
+def extrair_pos_tags(entidades):
+    nlp = spacy.load('pt_core_news_sm')
+    pos_tags = []
+    for entidade in entidades:
+        doc = nlp(entidade)
+        for token in doc:
+            if not token.is_punct and not token.is_space:
+                pos_tags.append(token.pos_)
+    return pos_tags
 
 # Leitura dos dados do arquivo fonte "folha8.Out.txt"
 def le_arquivo_fonte(nomearquivo):
@@ -15,53 +34,25 @@ def le_arquivo_fonte(nomearquivo):
         conteudo = arquivo.read()
         return conteudo
 
-# Função para quebrar por matérias
-def quebra_por_materias(folha):
-    materias = folha.split("<pub>")
-    return materias
-
-# 1. Calcular quantas publicações existem na base
-def calcular_total_publicacoes(materias):
-    return len(materias)
-
-# 2. Extrair as listas das tags
-def extrair_tags(materia):
-    tags = re.findall(r'tag:{.*?}', materia)
-    return tags
-
-# 3. Calcular as tags que ocorrem e quanto ocorrem
-def calcular_contagem_tags(materias):
-    contagem_tags = {}
-    for materia in materias:
-        tags_materia = extrair_tags(materia)
-        for tag in tags_materia:
-            if tag not in contagem_tags:
-                contagem_tags[tag] = 1
-            else:
-                contagem_tags[tag] += 1
-    return contagem_tags
-
-# Carrega o conteúdo do arquivo para variável folha8
+# Carregar o conteúdo do arquivo para variável folha8
 folha8 = le_arquivo_fonte("folha8.OUT.txt")
 
-# Quebra por matérias
-lista_materias = quebra_por_materias(folha8)
+# Realizar NER apenas em entidades entre chaves no texto
+entidades_chaves = realizar_ner_entre_chaves(folha8)
 
-# 1. Calcular quantas publicações existem na base
-total_publicacoes = calcular_total_publicacoes(lista_materias)
-print(f"Total de publicações: {total_publicacoes}")
+# Extrair POS tags das palavras nas tags {}
+pos_tags_entidades = extrair_pos_tags(entidades_chaves)
 
-# 3. Calcular as tags que ocorrem e quanto ocorrem
-contagem_tags = calcular_contagem_tags(lista_materias)
+# Contar frequência das classes gramaticais
+contagem_pos_tags = Counter(pos_tags_entidades)
 
-# 4. Contar o número total de '{}' no texto
-numero_chaves = folha8.count('{}')
-print(f"Número total de '{{}}': {numero_chaves}")
+# Plotar gráfico de barras com a frequência das classes gramaticais
+plt.figure(figsize=(10, 6))
+plt.bar(contagem_pos_tags.keys(), contagem_pos_tags.values())
+plt.title('Frequência de Classes Gramaticais das Palavras nas Tags')
+plt.xlabel('Classes Gramaticais')
+plt.ylabel('Frequência')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
-# 5. Contar o número total de ocorrências de todas as tags
-total_tags = sum(contagem_tags.values())
-print(f"Total de ocorrências de todas as tags: {total_tags}")
-
-
-# 4. Extrair a gama de dados do texto
-# Implemente a lógica para extrair a gama de dados aqui, se aplicável
