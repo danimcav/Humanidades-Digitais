8445100ccf041c925a087d7bf4603bef9fbe6861
danimcav <145656583+danimcav@users.noreply.github.com>
2024-02-14 10:56:33 +0000

    Add files via upload

--- TPC aula 09 Fev COM_UM.py
+++ TPC aula 09 Fev COM_UM.py
@@ -1,5 +1,5 @@
 from jjcli import *
 
-for n in range(13,17):
-    comando= f'wget -r -c -l 2 "https://www.comumonline.com/page/{n}/?s=entrevista"'
-    qxsystem (comando)
+for n in range(13, 17):
+    comando = f'wget --no-check-certificate -r -c -l 2 "https://www.comumonline.com/page/{n}/?s=entrevista"'
+    qxsystem(comando)
--- TPC aula 09 Fev NOS.py
+++ TPC aula 09 Fev NOS.py
@@ -1,5 +1,5 @@
 from jjcli import *
 
-for n in range(1088,1095):
-    comando= f'wget -r -c -l 2 "http://www.nos.uminho.pt/History.aspx?id={n}"'
-    qxsystem (comando)
+for n in range(1088, 1095):
+    comando = f'wget --no-check-certificate -r -c -l 2 "http://www.nos.uminho.pt/History.aspx?id={n}"'
+    qxsystem(comando)
